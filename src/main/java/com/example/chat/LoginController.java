d15ae2e8e25990348f9abb6d9bd99078d5f88837
reju-1 <rahmed1445@gmail.com>
2023-03-29 21:08:37 +0600

    login and register window GUI complete need Database connection

--- src/main/java/com/example/chat/LoginController.java
+++ src/main/java/com/example/chat/LoginController.java
@@ -1,4 +1,53 @@
 package com.example.chat;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class LoginController {
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField phoneNumber;
+
+    @FXML
+    void getLogin(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("home.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxmlLoader1.load());
+            //    stage.getIcons().add(new Image(getClass().getResourceAsStream("online-course.png")));
+            stage.setTitle("SignUp");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void goToRegisterWidow(MouseEvent event) {
+        try {
+            FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("register.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxmlLoader1.load());
+            //    stage.getIcons().add(new Image(getClass().getResourceAsStream("online-course.png")));
+            stage.setTitle("SignUp");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
--- src/main/java/com/example/chat/RegisterController.java
+++ src/main/java/com/example/chat/RegisterController.java
@@ -1,4 +1,58 @@
 package com.example.chat;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class RegisterController {
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private PasswordField userPassword;
+
+    @FXML
+    private TextField userPhone;
+
+    @FXML
+    void getRegister(ActionEvent event) {
+
+        try {
+            FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxmlLoader1.load());
+            //    stage.getIcons().add(new Image(getClass().getResourceAsStream("online-course.png")));
+            stage.setTitle("SignUp");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void goToLoginWindow(MouseEvent event) {
+
+        try {
+            FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(fxmlLoader1.load());
+            //    stage.getIcons().add(new Image(getClass().getResourceAsStream("online-course.png")));
+            stage.setTitle("SignUp");
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
 }
