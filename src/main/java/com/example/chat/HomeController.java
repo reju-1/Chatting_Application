45429c4b361c756308bacc0e1e5756f6a21bf382
reju-1 <rahmed1445@gmail.com>
2023-04-08 19:51:59 +0600

    icon are added

--- src/main/java/com/example/chat/HomeController.java
+++ src/main/java/com/example/chat/HomeController.java
@@ -1,4 +1,15 @@
 package com.example.chat;
 
-public class HomeController {
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable  {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+
 }
