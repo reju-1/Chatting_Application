23cc1ab14027a429ac8acda4f2ccc1f750816672
reju-1 <rahmed1445@gmail.com>
2023-04-08 23:08:53 +0600

    datas

--- src/main/java/com/example/chat/HomeController.java
+++ src/main/java/com/example/chat/HomeController.java
@@ -9,6 +9,7 @@ public class HomeController implements Initializable  {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+        
     }
 
 
---
+++ src/main/java/com/example/chat/model/Contact.java
@@ -0,0 +1,34 @@
+package com.example.chat.model;
+
+public class Contact {
+    private String number;
+    private String name;
+    private String imageUrl;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+
+
+}
